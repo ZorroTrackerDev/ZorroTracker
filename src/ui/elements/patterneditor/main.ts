faa81c2324e835b4c05cb5f52fac8c2f0b01db7e
Aurora <retrogreensnake@gmail.com>
2021-07-07 23:15:53 +0300

    convert pattern list to canvases and hugely optimize performance fix piano again

--- src/ui/elements/patterneditor/main.ts
+++ src/ui/elements/patterneditor/main.ts
@@ -2,23 +2,6 @@ import { loadFlag } from "../../../api/files";
 import { PatternIndex } from "../../../api/matrix";
 import { UIElement } from "../../../api/ui";
 
-type canvasElement = {
-	/**
-	 * The current pattern row ID
-	 */
-	pattern: number,
-
-	/**
-	 * The actual element
-	 */
-	element: HTMLCanvasElement,
-
-	/**
-	 * The 2D context of this canvas
-	 */
-	context: CanvasRenderingContext2D|null,
-};
-
 export class PatternEditor implements UIElement {
 	// various standard elements for the pattern editor
 	public element!:HTMLElement;
@@ -67,14 +50,15 @@ export class PatternEditor implements UIElement {
 			const scroll = (delta:number) => {
 				// change the scrolling position
 				this.scrollPosition = Math.round((delta * 0.03) + this.scrollPosition);
+				const blank = Math.ceil(this.scrollHeight / 50);
 
 				// try to clamp to minimim position
-				if(this.scrollPosition <= -16) {
-					this.scrollPosition = -16;
+				if(this.scrollPosition <= -blank) {
+					this.scrollPosition = -blank;
 
 				} else {
 					// calculate the maximum position
-					const max = (this.index.matrixlen * this.index.patternlen) + 16 - Math.floor(this.scrollHeight / this.dataHeight);
+					const max = (this.index.matrixlen * this.index.patternlen) + blank - Math.floor(this.scrollHeight / this.dataHeight);
 
 					// try to clamp to max position
 					if(this.scrollPosition > max) {
@@ -88,13 +72,56 @@ export class PatternEditor implements UIElement {
 
 			// add handler for vertical scrolling
 			this.scrollwrapper.addEventListener("wheel", (e) => {
+				if(e.deltaX) {
+					// there is horizontal movement, translate to a CSS variable
+					this.horizScroll = e.deltaX < 0 ?
+						Math.min(this.channelPositionsLeft.length - 1, this.horizScroll + 1) :
+						Math.max(0, this.horizScroll - 1);
+
+					// update canvas scrolling
+					this.canvas.forEach((c) => c.updateHoriz(this));
+
+					// update each channel header too
+					for(let i = this.index.channels.length;i >= 0;--i){
+						(this.scrollwrapper.children[i] as HTMLDivElement)
+							.style.transform = "translateX(-"+ this.channelPositionsLeft[this.horizScroll] +"px)"
+					}
+				}
+
 				if(e.deltaY) {
 					// there is vertical movement, translate it into a CSS variable
 					scroll(e.deltaY);
 				}
 			}, { passive: false, });
 
+			let timeout:null|NodeJS.Timeout = null;
+
+			// when window resizes, make sure to change scroll position as well
+			window.addEventListener("resize", () => {
+				// if previous timeout was defined, clear it
+				if(timeout) {
+					clearTimeout(timeout);
+				}
+
+				// create a new timeout for updating scrolling and pattern amounts
+				timeout = setTimeout(() => {
+					// uådate scrolling height
+					this.scrollHeight = this.scrollwrapper.getBoundingClientRect().height - 30;
+
+					// remove old canvases
+					this.canvas.forEach((c) => c.element.parentElement?.removeChild(c.element));
+
+					// refresh the amount of patterns
+					this.refreshPatternAmount();
+
+					// reload
+					this.handleScrolling();
+				}, 25);
+			});
+
+			// initialize canvas
 			this.handleScrolling();
+			this.canvas.forEach((c) => c.clear());
 
 			// initialize canvases
 			setTimeout(() => {
@@ -109,12 +136,12 @@ export class PatternEditor implements UIElement {
 	 * @param row The row number to calculcate
 	 * @returns A string representing the row number
 	 */
-	private getRowNumber!: (row:number) => string;
+	public getRowNumber!: (row:number) => string;
 
 	/**
 	 * The number of pixels for the height of each data element
 	 */
-	private dataHeight = 19;
+	public dataHeight = 19;
 
 	/**
 	 * Store the vertical scroll position of channel datas
@@ -136,17 +163,22 @@ export class PatternEditor implements UIElement {
 	/**
 	 * This is a list of all the channel x-positions from left. This helps canvases get lined up.
 	 */
-	private channelPositionsLeft!:number[];
+	public channelPositionsLeft!:number[];
 
 	/**
 	 * This is a list of all the channel x-positions from right. This helps canvases get lined up.
 	 */
-	private channelPositionsRight!:number[];
+	public channelPositionsRight!:number[];
+
+	/**
+	 * This defines which horizontal scroll position is going to be read
+	 */
+	public horizScroll = 0;
 
 	/**
 	 * This is the total width of the render area
 	 */
-	private totalWidth!:number;
+	public totalWidth!:number;
 
 	/**
 	 * Helper function to initialize empty channel content for each defined channel
@@ -219,26 +251,24 @@ export class PatternEditor implements UIElement {
 		this.canvas = [];
 
 		// calculate the amount of rows needed to display
-		const amount = 3;				// super cool temporary advanced math operation
+		const amount = Math.ceil(((this.scrollHeight / this.dataHeight) + (this.visibleSafeHeight * 2)) / this.index.patternlen);
 
 		// generate each row
-		for(let row = 0;row < amount; row++) {
-			// create the element and give it classes
-			const x = document.createElement("canvas");
-			x.width = this.totalWidth;
-			x.height = this.dataHeight * this.index.patternlen;
+		for(let row = 0;row <= amount; row++) {
+			// create a new canvas
+			const x = new PatternCanvas(this.totalWidth, this.dataHeight * this.index.patternlen, this.index.patternlen, this.index.channels.length);
+			this.scrollwrapper.appendChild(x.element);
+			this.canvas.push(x);
 
-			x.style.top = "-10000px";
-			x.classList.add("patterncanvas");
-			this.scrollwrapper.appendChild(x);
-			this.canvas.push({ element: x, context: x.getContext("2d"), pattern: -1, });
+			// update horizontal scrolling
+			x.updateHoriz(this);
 		}
 	}
 
 	/**
 	 * Container for each canvas element
 	 */
-	private canvas!:canvasElement[];
+	private canvas!:PatternCanvas[];
 
 	/**
 	 * Handle scrolling by updating positions
@@ -248,9 +278,11 @@ export class PatternEditor implements UIElement {
 		const [ rangeMin, rangeMax, ] = this.getVisibleRange();
 
 		// calculate the active pattern
-		const middle = this.scrollPosition + Math.round(this.scrollHeight / 2 / this.dataHeight);
-		const pat = Math.min(this.index.matrixlen - 1,
-			Math.max(0, Math.round((middle - (this.index.patternlen / 1.75)) / this.index.patternlen)));
+		const middle = this.scrollPosition + Math.round(this.scrollHeight / this.dataHeight / 2);
+		const pat = Math.min(this.index.matrixlen - 1, Math.max(0, Math.round((middle - (this.index.patternlen / 1.75)) / this.index.patternlen)));
+
+		// calculate the number of rows on screen at once
+		const rowsPerScreen = Math.ceil((this.scrollHeight - 30) / this.dataHeight);
 
 		// run for each visible row
 		for(let r = rangeMin;r <= rangeMax; r++) {
@@ -258,10 +290,22 @@ export class PatternEditor implements UIElement {
 			const cv = this.canvas[(this.canvas.length + r) % this.canvas.length];
 
 			// update canvas y-position
-			cv.element.style.top = ((((r * this.index.patternlen) - this.scrollPosition) * this.dataHeight) + 30) +"px";
+			const offsetTop = ((r * this.index.patternlen) - this.scrollPosition);
+			cv.element.style.top = ((offsetTop * this.dataHeight) + 30) +"px";
 
-			// fully render because lazy and testing
-			this.renderPattern(cv, r, r === pat, r >= 0 && r < this.index.matrixlen);
+			// invalidate layout if it is not the same row or active status don't match
+			if(cv.pattern !== r || (r === pat) !== cv.active) {
+				cv.active = r === pat;
+				cv.pattern = r;
+				cv.clear();
+			}
+
+			// check if this is visible
+			if(r >= 0 && r < this.index.matrixlen) {
+				cv.renderPattern(this,
+					Math.max(0, -offsetTop - this.visibleSafeHeight),
+						Math.min(this.index.patternlen, rowsPerScreen + this.visibleSafeHeight - offsetTop));
+			}
 		}
 	}
 
@@ -269,7 +313,7 @@ export class PatternEditor implements UIElement {
 	 * How many rows that can be hidden, but will still make the visible range larger.
 	 * This is used so that the user doesn't see the pattern list elements loading.
 	 */
-	private visibleSafeHeight = 6;
+	private visibleSafeHeight = 3;
 
 	/**
 	 * Helper function to get the visible range of patterns
@@ -281,75 +325,126 @@ export class PatternEditor implements UIElement {
 			Math.floor((this.scrollPosition + this.visibleSafeHeight + (this.scrollHeight / this.dataHeight)) / this.index.patternlen),
 		];
 	}
+}
 
-	private renderPattern(cv:canvasElement, row:number, active:boolean, valid:boolean) {
-		cv.pattern = row;
+class PatternCanvas {
+	public element:HTMLCanvasElement;
+	public pattern:number;
+	public active:boolean;
+	private patternlen:number;
+	private channels:number;
 
-		if(!cv.context) {
-			console.error("failed to capture 2D context for row "+ row);
+	constructor(width:number, height:number, patternlen:number, channels:number) {
+		// create the element and give it classes
+		this.element = document.createElement("canvas");
+		this.element.width = width;
+		this.element.height = height;
+
+		this.element.style.top = "-10000px";
+		this.element.classList.add("patterncanvas");
+
+		this.pattern = -1;
+		this.channels = channels;
+		this.patternlen = patternlen;
+		this.active = false;
+
+		this.rendered = Array<boolean>(patternlen);
+		this.clear();
+	}
+
+	/**
+	 * Update horizontal scrolling of canvas
+	 */
+	public updateHoriz(parent:PatternEditor) {
+		this.element.style.left = -parent.channelPositionsLeft[parent.horizScroll] +"px";
+	}
+
+	private rendered:boolean[];
+
+	public clear() {
+		// get the 2D context and check if valid
+		const ctx = this.element.getContext("2d");
+
+		if(!ctx) {
 			// wtf fail
+			console.error("failed to capture 2D context for pattern "+ this.pattern);
 			return;
 		}
 
-		const ctx = cv.context;
-
-		if(!valid) {
-			// if outside of bounds, just fill with black
-			ctx.fillStyle = "#000";
-			ctx.fillRect(0, 0, cv.element.width, cv.element.height);
-			return;
-		}
-
-		// draw background
-		ctx.fillStyle = active ? "#262627" : "#1E1E1E";
-		ctx.fillRect(0, 0, cv.element.width, cv.element.height);
-
-		// draw borders
+		// just clear the entire deal
 		ctx.fillStyle = "#000";
+		ctx.fillRect(0, 0, this.element.width, this.element.height);
 
-		this.channelPositionsRight.forEach((left) => {
-			ctx.fillRect(left, 0, 4, cv.element.height);
-		});
+		for(let i = 0;i < this.patternlen;i ++){
+			this.rendered[i] = false;
+		}
+	}
+
+	public renderPattern(parent:PatternEditor, start:number, end:number) {
+		// get the 2D context and check if valid
+		const ctx = this.element.getContext("2d");
+
+		if(!ctx) {
+			// wtf fail
+			console.error("failed to capture 2D context for pattern "+ this.pattern);
+			return;
+		}
 
 		// prepare text
 		ctx.font = "10pt 'Roboto Mono'";
-		ctx.fillStyle = active ? "#949494" : "#686868";
 
-		// draw pattern indices
-		for(let r = 0;r < this.index.patternlen;r ++) {
-			ctx.fillText(this.getRowNumber(r), this.channelPositionsLeft[0] + 3, 14 + (r * this.dataHeight));
-		}
-
-		// draw all other elements
-		for(let r = 0;r < this.index.patternlen;r ++) {
-			const top = 14 + (r * this.dataHeight);
-
-			for(let c = 0;c < this.index.channels.length;c ++) {
-				const left = this.channelPositionsLeft[c + 1];
-
-				for(let i = 0;i < 5;i ++){
-					if(c & 1) {
-						let text = "";
-						switch(i) {
-							case 0: text = "C#6"; break;
-							case 1: text = "2F"; break;
-							case 2: text = "11"; break;
-							case 3: text = "WQ"; break;
-							case 4: text = "DD"; break;
-						}
-
-						ctx.fillStyle = active ? this.channelElementColors[i] : "#686868";
-						ctx.fillText(text, left + this.channelElementOffsets[i], top);
-
-					} else {
-						ctx.fillStyle = active ? "#616161" : "#404040";
-						ctx.fillText(i === 0 ? "---" : "--", left + this.channelElementOffsets[i], top);
-					}
-				}
+		for(let r = start;r <= end;r ++) {
+			if(!this.rendered[r]) {
+				this.renderRow(r, ctx, parent);
+				this.rendered[r] = true;
 			}
 		}
 	}
 
 	private channelElementOffsets = [ 3, 31, 50, 70, 87, ];
 	private channelElementColors = [ "#b7b7b7", "#7e81a5", "#62ab4a", "#b16f6f", "#bba6a1", ];
+
+	private renderRow(row:number, ctx:CanvasRenderingContext2D, parent:PatternEditor) {
+		const top = 14 + (row * parent.dataHeight);
+
+		// draw background
+		ctx.fillStyle = this.active ? "#262627" : "#1E1E1E";
+		ctx.fillRect(0, top - 14, this.element.width, parent.dataHeight);
+
+		// draw borders
+		ctx.fillStyle = "#000";
+
+		parent.channelPositionsRight.forEach((left) => {
+			ctx.fillRect(left, top - 14, 4, parent.dataHeight);
+		});
+
+		// render pattern index
+		ctx.fillStyle = this.active ? "#949494" : "#686868";
+		ctx.fillText(parent.getRowNumber(row), parent.channelPositionsLeft[0] + 3, top);
+
+		// render all other text
+		for(let c = 0;c < this.channels;c ++) {
+			const left = parent.channelPositionsLeft[c + 1];
+
+			for(let i = 0;i < 5;i ++){
+				if(c & 1) {
+					let text = "";
+					switch(i) {
+						case 0: text = "C#6"; break;
+						case 1: text = "2F"; break;
+						case 2: text = "11"; break;
+						case 3: text = "WQ"; break;
+						case 4: text = "DD"; break;
+					}
+
+					ctx.fillStyle = this.active ? this.channelElementColors[i] : "#686868";
+					ctx.fillText(text, left + this.channelElementOffsets[i], top);
+
+				} else {
+					ctx.fillStyle = this.active ? "#616161" : "#404040";
+					ctx.fillText(i === 0 ? "---" : "--", left + this.channelElementOffsets[i], top);
+				}
+			}
+		}
+	}
 }

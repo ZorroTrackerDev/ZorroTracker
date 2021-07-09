9ed6db45b96b6abcce77600a364e0d60c195652b
Aurora <retrogreensnake@gmail.com>
2021-07-10 00:28:47 +0300

    load fonts for pattern editor from local filesystem

--- src/ui/misc/theme.ts
+++ src/ui/misc/theme.ts
@@ -1,6 +1,7 @@
 import { ZorroEvent, ZorroEventEnum } from "../../api/events";
 import { loadFiles } from "../../api/files";
 import { ThemeConfig } from "../../api/theme";
+import path from "path";
 
 /**
  * The event dispatcher for LoadTheme event
@@ -31,6 +32,8 @@ export function loadTheme(config:ThemeConfig):void {
 	// TODO: figure out how to merge it all
 	theme = data[0];
 
+	fixItems<string>(_folder, (u) => u.replace("%folder%", path.join(config.entry).replace(/\\/g, "/")));
+
 	// inform theme is loaded
 	update().catch(console.error);
 }
@@ -46,3 +49,48 @@ let _cfg:ThemeConfig;
 export function reloadTheme(): void {
 	loadTheme(_cfg);
 }
+
+/**
+ * Function for fixing item data, such as text replacement
+ *
+ * @param items The list of items to modify
+ * @param fn The function for converting items data
+ */
+function fixItems<T>(items:string[], fn:(item:T) => T) {
+	// run for every single item
+	items.forEach((i) => {
+		try {
+			// get the item indexes by splitting the string
+			const a = i.split(".");
+			let head:{ [key: string]: unknown, } = theme as { [key: string]: unknown, };
+
+			// run for every index in the array
+			for(let i = 0;i < a.length;i ++) {
+				if(typeof head === "object" && head[a[i]]) {
+					if(i < a.length - 1) {
+						// if index in head exists, add it as our new head
+						head = head[a[i]] as { [key: string]: unknown, };
+
+					} else {
+						try {
+							// this is the last item, handle it now
+							head[a[i]] = fn(head[a[i]] as T);
+
+						} catch(ex) {
+							/* ignore */
+						}
+					}
+				}
+			}
+		} catch(ex) {
+			/* ignore */
+		}
+	});
+}
+
+/**
+ * All the %folder% conversion points
+ */
+const _folder = [
+	"pattern.worker.font.source",
+];

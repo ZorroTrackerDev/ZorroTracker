3ca5192c354e456a6c0e3ba7315d00f6f5f5cada
Aurora <retrogreensnake@gmail.com>
2021-07-30 00:08:57 +0300

    fix some npm scripts and mixed space/tab things

--- bin/build-packages.js
+++ bin/build-packages.js
@@ -7,31 +7,31 @@ const os = require("os");
 const paths = ["../src/scripts/chips", "../src/scripts/drivers", "../src/scripts/audio", ];
 
 for (const path of paths) {
-    const dir = resolve(__dirname, path);
+	const dir = resolve(__dirname, path);
 
-    fs.readdirSync(dir).forEach(function(mod) {
-        const modPath = join(dir, mod);
+	fs.readdirSync(dir).forEach(function(mod) {
+		const modPath = join(dir, mod);
 
-        // ensure path has package.json
-        if (!fs.existsSync(join(modPath, "package.json"))) {
-            return;
-        }
+		// ensure path has package.json
+		if (!fs.existsSync(join(modPath, "package.json"))) {
+			return;
+		}
 
-        // npm binary based on OS
-        const npmCmd = os.platform().startsWith("win") ? "npm.cmd" : "npm";
+		// npm binary based on OS
+		const npmCmd = os.platform().startsWith("win") ? "npm.cmd" : "npm";
 
-        const install = cp.spawn(npmCmd, ["install", ], {
-            env: process.env,
-            cwd: modPath,
-            stdio: "inherit",
-        });
+		const install = cp.spawn(npmCmd, ["install", ], {
+			env: process.env,
+			cwd: modPath,
+			stdio: "inherit",
+		});
 
-        install.on("exit", () => {
-            cp.spawn(npmCmd, ["run", "build", ], {
-                env: process.env,
-                cwd: modPath,
-                stdio: "inherit",
-            });
-        })
-    })
+		install.on("exit", () => {
+			cp.spawn(npmCmd, ["run", "build", ], {
+				env: process.env,
+				cwd: modPath,
+				stdio: "inherit",
+			});
+		})
+	})
 }
